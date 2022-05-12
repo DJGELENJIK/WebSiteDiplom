d5240e6fd490c12d6cc012943fb7621783851ec3
DJGELENJIK <suehoh78@bk.ru>
2022-05-12 03:39:45 +0300

    5

---
+++ resources/views/product.blade.php
@@ -0,0 +1,13 @@
+@extends('master',['file'=>'product'])
+
+@section('title', 'Товар')
+
+@section('content')
+    <div class="starter-template">
+        <h1>iPhone X 64GB</h1>
+        <h2>Мобильные телефоны</h2>
+        <p>Цена: <b>71990 ₽</b></p>
+        <img src="http://internet-shop.tmweb.ru/storage/products/iphone_x.jpg">
+        <p>Отличный продвинутый телефон с памятью на 64 gb</p>
+</div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -2,17 +2,19 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+
+Route::get('/','App\Http\Controllers\MainController@index' )->name('index');
+
+Route::get('/categories','App\Http\Controllers\MainController@categories')->name('categories');
+
+Route::get('/{category}','App\Http\Controllers\MainController@category' )->name('category');
+
+Route::get('/{category}/{product?}','App\Http\Controllers\MainController@product')->name('product');
+
+Route::get('/basket','App\Http\Controllers\MainController@basket')->name('basket');
+
+Route::get('/basket/place','App\Http\Controllers\MainController@basketPlace')->name('basket-place');
+
+
