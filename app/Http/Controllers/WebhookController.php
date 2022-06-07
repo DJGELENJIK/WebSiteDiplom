9ce3d278af57dad8065990319b7e893dbccf9a6c
Antoshkin.P <plazmakek12@gmail.com>
2022-06-07 20:36:42 +0300

    Update WebhookController.php

--- app/Http/Controllers/WebhookController.php
+++ app/Http/Controllers/WebhookController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Order;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
 
@@ -9,5 +10,10 @@ class WebhookController extends Controller
 {
     public function index(Request $request) {
         Log::debug($request->all());
+        $public = explode('|', $request->input('callback_query')['data'])[0];
+        $secret_key = explode('|', $request->input('callback_query')['data'])[1];
+        $order = Order::where('secret_Key', $secret_key)->first();
+        $order->status = explode('|', $request->input('callback_query')['data'])[0];
+        $order->save();
     }
 }
