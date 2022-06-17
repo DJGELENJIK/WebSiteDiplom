ff4bdd4bf94dfe8f8d76998f0e2b18dc5b46662b
Antoshkin.P <plazmakek12@gmail.com>
2022-06-17 03:56:47 +0300

    Update TelegramSubscriber.php

--- app/Listeners/TelegramSubscriber.php
+++ app/Listeners/TelegramSubscriber.php
@@ -39,7 +39,7 @@ class TelegramSubscriber
             ]
         ];
 
-        $this->telegram->sendButtons(env('REPORT_TELEGRAM_ID'), (string)view('telegramViews.OrderReport', $data), $buttons);
+        $this->telegram->sendButtons(env('ORDER_TELEGRAM_ID'), (string)view('telegramViews.OrderReport', $data), $buttons);
 
     }
 
